4e9539e6588a8c1b5d9e8e1229aaac7a474fb313
Steven Ang <spikegreen2@gmail.com>
2024-09-02 21:04:35 +0800

    Add functionality

---
+++ main.js
@@ -0,0 +1,75 @@
+const cardList = document.querySelector(".card-list");
+const toggleButtons = document.querySelector(".toggle-buttons-list");
+
+let currentActiveButton = null;
+let previousActiveButton = null;
+
+const renderCards = async (type) => {
+  const response = await fetch("data.json");
+  const data = await response.json();
+
+  if (cardList.innerHTML !== "") cardList.innerHTML = "";
+
+  for (const item of data) {
+    const { title, timeframes } = item;
+
+    const currentWeek =
+      type === "weekly"
+        ? timeframes.weekly.current
+        : type === "daily"
+        ? timeframes.daily.current
+        : timeframes.monthly.current;
+    const previousWeek =
+      type === "weekly"
+        ? timeframes.weekly.previous
+        : type === "daily"
+        ? timeframes.daily.previous
+        : timeframes.monthly.previous;
+
+    const tagClassName = title.toLowerCase().split(" ").join("-");
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+      <div class="card-background ${tagClassName}"></div>
+      <div class="card-content">
+        <div class="card-content-labels">
+          <div class="card-label">${title}</div>
+          <img
+            src="images/icon-ellipsis.svg"
+            alt="Ellipsis icon"
+            class="icon-ellipsis"
+          />
+        </div>
+        <div class="card-content-weeks">
+          <p class="current-week">${currentWeek}hrs</p>
+          <p class="previous-week">Last week - ${previousWeek}hrs</p>
+        </div>
+      </div>
+    `;
+    cardList.append(card);
+  }
+};
+
+const handleToggle = (event) => {
+  event.stopPropagation();
+
+  previousActiveButton = currentActiveButton;
+  previousActiveButton.classList.toggle("active");
+
+  currentActiveButton = event.target;
+  currentActiveButton.classList.toggle("active");
+  
+  renderCards(currentActiveButton.textContent.toLowerCase());
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  currentActiveButton = toggleButtons.children.item(1);
+  currentActiveButton.classList.toggle("active");
+
+  renderCards("weekly");
+});
+
+for (const li of toggleButtons.children) {
+  li.addEventListener("click", handleToggle);
+}
